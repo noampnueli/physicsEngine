0cd8f5cc70ff5b44952e67d90d276d3211b1e4fe
Noam <noam.pnueli@gmail.com>
2017-07-22 21:50:19 +0300

    borked

--- main.cpp
+++ main.cpp
@@ -11,15 +11,15 @@ int main(int argc, char** argv)
 
     Box* box1 = new Box(10, vector2d(400, 0), vector2d(0, 0), 100, 100);
     //Box* box2 = new Box(50, vector2d(350, 200), vector2d(0, -10), 100, 100);
-    Circle* circle1 = new Circle(60, vector2d(200, 0), vector2d(0, 0), 50);
-//    Circle* circle2 = new Circle(0, vector2d(200, 400), vector2d(0, 0), 50);
-    Floor* floor = new Floor();
-    floor->y_loc = 500;
+    Circle* circle1 = new Circle(60, vector2d(200, 0), vector2d(30, 0), 50);
+    Circle* circle2 = new Circle(60, vector2d(500, 0), vector2d(-30, 0), 50);
+    //Floor* floor = new Floor();
+    //floor->y_loc = 500;
 //    stage.push_back(box1);
     //stage.push_back(box2);
     stage.push_back(circle1);
-//    stage.push_back(circle2);
-    stage.push_back(floor);
+    stage.push_back(circle2);
+    //stage.push_back(floor);
     //stage.push_back(circle3);
 
     Physics physics = Physics(stage);
