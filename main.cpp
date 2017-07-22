a2b6e88683aea81c4bab7c845118225af96aa67e
Shahar Rose <rose.shahar@gmail.com>
2017-07-22 21:08:01 +0300

    falling circles testing

--- main.cpp
+++ main.cpp
@@ -10,9 +10,8 @@ int main(int argc, char** argv)
 
     //Box* box1 = new Box(10, vector2d(400, 0), vector2d(0, 0), 100, 100);
     //Box* box2 = new Box(50, vector2d(350, 200), vector2d(0, -10), 100, 100);
-    Circle* circle1 = new Circle(60, vector2d(200, 0), vector2d(30, 0), 50);
-    Circle* circle2 = new Circle(10, vector2d(700, 0), vector2d(-30, 0), 50);
-    Circle* circle3 = new Circle(200, vector2d(300, 30), vector2d(-70, 0), 20);
+    Circle* circle1 = new Circle(60, vector2d(200, 0), vector2d(0, 0), 50);
+    Circle* circle2 = new Circle(0, vector2d(200, 400), vector2d(0, 0), 50);
     //stage.push_back(box1);
     //stage.push_back(box2);
     stage.push_back(circle1);
