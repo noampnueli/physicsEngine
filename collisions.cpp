fa89a6fa7fdd09df195a5ad5d9de9480a5880bfa
Shahar Rose <rose.shahar@gmail.com>
2017-07-23 17:41:03 +0300

    aabb template not ready

--- collisions.cpp
+++ collisions.cpp
@@ -9,22 +9,28 @@ Manifold collision_overlap(Collider* coll1 , Collider* coll2)
 {
     if(coll1->type == AABB_c && coll2->type == AABB_c)
     {
-        AABB* n_coll2 = (AABB *) coll2;
-        AABB* n_coll1 = (AABB *) coll1;
 
-        double dx1 = n_coll2->min.x - n_coll1->max.x;
-        double dy1 = n_coll2->min.y - n_coll1->max.y;
-        double dx2 = n_coll1->min.x - n_coll2->max.x;
-        double dy2 = n_coll1->min.y - n_coll2->max.y;
+        AABB* abox = (AABB *) coll1;
+        AABB* bbox = (AABB *) coll2;
 
-        if(dx1 > 0 || dy1 > 0)
-            return Manifold(-1, vector2d(0, 0), nullptr, nullptr);
+        double a_extent = (abox->max.x - abox->min.x) / 2;
+        double b_extent = (bbox->max.x - bbox->min.x) / 2;
 
-        if(dx2 > 0 || dy2 > 0)
-            return Manifold(-1, vector2d(0, 0), nullptr, nullptr);
+        double x_overlap = a_extent + b_extent - abs(n.x);
 
-        // TODO: Fill with actual values
-        return Manifold(0, vector2d(0, 0), nullptr, nullptr);
+//        double dx1 = n_coll2->min.x - n_coll1->max.x;
+//        double dy1 = n_coll2->min.y - n_coll1->max.y;
+//        double dx2 = n_coll1->min.x - n_coll2->max.x;
+//        double dy2 = n_coll1->min.y - n_coll2->max.y;
+//
+//        if(dx1 > 0 || dy1 > 0)
+//            return Manifold(-1, vector2d(0, 0), nullptr, nullptr);
+//
+//        if(dx2 > 0 || dy2 > 0)
+//            return Manifold(-1, vector2d(0, 0), nullptr, nullptr);
+//
+//        // TODO: Fill with actual values
+//        return Manifold(0, vector2d(0, 0), nullptr, nullptr);
     }
     else if(coll1->type == Circle_c && coll2->type == Circle_c)
     {
